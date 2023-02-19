295879da02ec56b8a4286aa4273da8b3662369ee
rishabsantosh <61828279+rishabsantosh@users.noreply.github.com>
2023-02-19 13:38:17 +0530

    Update main.py

--- py-app/main.py
+++ py-app/main.py
@@ -1,9 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
+import sys
+# from logmine_pkg.run import run
+
+# The below statement creates a WSGI application, it's required because it's a standard to communicate between web server and the web application
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello World from Pycon Flask"
+@app.route('/') # Decorator
+def welcome():
+    return render_template("index.html")
+
+@app.route('/cluster', methods = ['GET']) # Default value also is 'GET'
+def logs():
+    sys.argv
+    os.system("./logmine Apache/Apache_2k.log -m0.5 > cout.txt")
+    txt_file = open("cout.txt")
+    lines = txt_file.readlines()
+    out = ""
+
+    for line in lines:
+        out = out + line + '\n'
+        # Try <br>, search how to add next line from python to html
+
+    txt_file.close()
+
+    return render_template('lghtml.html', dataToRender = out)
+
 
 if __name__ == "__main__":
     # Only for debugging while developing
